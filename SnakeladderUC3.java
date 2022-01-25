ec9def116d02a9eac9cd11680c87db21b672eeec
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-25 08:24:07 +0530

    Adding functionality for checking option for no play,ladder& snake

---
+++ SnakeladderUC3.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.snakeladder;
+import java.util.Random;
+
+public class SnakeladderUC3 {
+	public static final int startPosn = 0;
+	public static final int endPosn = 100;
+        private static int newPosition = startPosn;
+        private static int option = 0;
+        private static int diceNumber = 0;
+
+        public static int throwDice(){
+            Random r=new Random();
+            int random = r.nextInt(6)+1;
+          //int r1 = 1 + (random % 6);
+            return random;
+         }
+
+        public static int optionCheck(){
+            Random r1 = new Random();
+            int random1 =r1.nextInt(3)+1;
+            return random1;
+          }
+
+ 	public static void main(String[] args) {
+
+                diceNumber=throwDice();
+                option=optionCheck();
+
+                if (option == 2){// for ladder //
+		  newPosition = newPosition+diceNumber;
+                 }
+		else if (option == 3){// for snake //
+		  newPosition = newPosition-diceNumber;
+                 }
+                else if (option == 0){// for no play //
+                  newPosition=newPosition;
+                 }
+
+                System.out.println("Starting Position : "+ startPosn);
+		System.out.println("Ending Position : "+ endPosn);
+                System.out.println("New Position : "+ newPosition);
+	}
+   }
