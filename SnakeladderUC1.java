207ed52d1eecc6c1dc48d40ca5e4e3c009cc95e6
vaibhav rajuskar <vaibhavrajuskar54@gmail.com>
2022-01-25 08:22:24 +0530

    initialising start and end position

---
+++ SnakeladderUC1.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeladderUC1 {
+	
+	public static final int START_POSITION= 0;
+	public static final int END_POSITION = 100;
+	  
+	  
+	  public static void main(String[] args) {
+
+         System.out.println("Starting Position : "+ START_POSITION);
+         System.out.println("Ending Position : "+ END_POSITION);
+		}
+
+}
+
+
+
